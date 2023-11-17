204cefda1ecac7cc40d0ee0ef24a047b56eb4db1
akidon0000 <a@art.jp>
2023-11-17 11:46:27 +0900

    [1] 不要なコメントアウトを削除

--- tokumemo_plus/lib/app.dart
+++ tokumemo_plus/lib/app.dart
@@ -3,10 +3,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:tokumemo_plus/main.dart';
 
-// import 'presentation/pages/main/main_page.dart';
-// import 'presentation/widgets/have_scroll_bar_behavior.dart';
-// import 'utils/navigator_key_provider.dart';
-
 class App extends ConsumerWidget {
   const App({super.key});
 
@@ -14,8 +10,6 @@ class App extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'テンプレアプリ',
-      // navigatorKey: ref.watch(navigatorKeyProvider),
-      // scrollBehavior: const HaveScrollBarBehavior(),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
