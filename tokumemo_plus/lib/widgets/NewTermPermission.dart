617fe6c3c60263bf5b2a46c17f330029ecacdd6c
marlion01 <fantand.greeeen.mass@gmail.com>
2023-11-17 12:40:39 +0900

    "規約同意画面のボタンとスクロールビューの作成"

--- tokumemo_plus/lib/widgets/NewTermPermission.dart
+++ tokumemo_plus/lib/widgets/NewTermPermission.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 class NewTermsPermissionView extends StatelessWidget{
+  String TermText="";
   @override
   Widget build(BuildContext context){
     return Scaffold(
         body: Center(
-          child: Text("規約同意画面"),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('Button'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: (){},
+                      child: const Text('Button')
+                  )
+                ],
+              )
+            ],
+          )
         ),
     );
   }
