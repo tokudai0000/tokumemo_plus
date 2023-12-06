ed9b71dd5c6657525b0611ba066245e2b4075a6e
akidon0000 <a@art.jp>
2023-12-07 01:26:23 +0900

    [14] webviewのサンプルを作成

--- tokumemo_plus/lib/features/user_agreement/pages/user_agreement_page.dart
+++ tokumemo_plus/lib/features/user_agreement/pages/user_agreement_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class UserAgreementPage extends ConsumerWidget {
   const UserAgreementPage({super.key});
@@ -9,16 +10,10 @@ class UserAgreementPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('user_agreement_page'),
-      ),
-      body: Center(
-        child: Text(
-          'user_agreement_page',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: WebViewWidget(
+        controller: WebViewController()
+          ..loadRequest(Uri.parse("https://tokumemo.notion.site/81a6f545f13b409c8b6298ad5e03e992")),
       ),
     );
   }
